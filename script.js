7976fc8eb073a4b53ee8fa7e21b6b3cffd6e12fd
Marc <marcdsp@gmail.com>
2022-12-27 08:55:46 -0600

    completed first draft

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
-var humanScore=0;
-var computerScore=0;
+var humanScore = 0;
+var compScore = 0;
 
 
-/*// get randomized computer choice
-function computerWeapon() {
+function humanWeapon(parameter) {
+    let humanChoice = "" + parameter + "";
     const options = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * options.length);
-    console.log(random, options[random]);
-    }
+    console.log(options[random]);
+    console.log(humanChoice)
 
-
-
-    const random = Math.floor(Math.random() * months.length);
-    console.log(random, months[random]);
-*/
-
-//function to catch the users choice
-function humanWeapon(parameter) {
-    if (humanScore < 5 || computerScore < 5)  {
-        let humanChoice = "" + parameter + "";
-        const options = ["Rock", "Paper", "Scissors"];
-        const random = Math.floor(Math.random() * options.length);
-        console.log(options[random]);
-        console.log(humanChoice)
-
-    if (options[random] == humanChoice){
+    if (options[random] == humanChoice) {
         alert("It's a TIE!!!");
         winner = "Tie"
- 
-    }
-    else if (options[random] == "Rock" && humanChoice == "Scissors"){
+        checkScore()
+    } else if (options[random] == "Rock" && humanChoice == "Scissors") {
         alert("You Chose " + humanChoice + " and the computer chose " + options[random] + "....You LOSE!!!");
-        winner = "Computer"
- 
-    }
-    else if (options[random] == "Rock" && humanChoice == "Paper"){
+        compScore += 1
+        checkScore()
+    } else if (options[random] == "Rock" && humanChoice == "Paper") {
         alert("You Chose " + humanChoice + " and the computer chose " + options[random] + "....You WIN!!!");
-        winner = "Human"
- 
-    }
-    else if (options[random] == "Paper" && humanChoice == "Rock"){
+        humanScore += 1
+        checkScore()
+    } else if (options[random] == "Paper" && humanChoice == "Rock") {
         alert("You Chose " + humanChoice + " and the computer chose " + options[random] + "....You LOSE!!!");
-        winner = "Computer"
- 
-    }
-    else if (options[random] == "Paper" && humanChoice == "Scissors"){
+        compScore += 1
+        checkScore()
+    } else if (options[random] == "Paper" && humanChoice == "Scissors") {
         alert("You Chose " + humanChoice + " and the computer chose " + options[random] + "....You WIN!!!")
-        winner = "Human"
- 
-    }
-    else if (options[random] == "Scissors" && humanChoice == "Paper"){
+        humanScore += 1
+        checkScore()
+    } else if (options[random] == "Scissors" && humanChoice == "Paper") {
         alert("You Chose " + humanChoice + " and the computer chose " + options[random] + "....You LOSE!!!")
-        winner = "Computer"
- 
-    }
-    else if (options[random] == "Scissors" && humanChoice == "Rock"){
+        compScore += 1
+        checkScore()
+    } else if (options[random] == "Scissors" && humanChoice == "Rock") {
         alert("You Chose " + humanChoice + " and the computer chose " + options[random] + "....You WIN!!!")
-        winner = "Human"
- 
-    } 
-}
+        humanScore += 1
+        checkScore()
     }
+}
 
+function checkScore() {
+    if (humanScore != 5 && compScore != 5) {
+        document.getElementById("theirScore").innerHTML = "Computer Score is " + compScore +"";
+        document.getElementById("yourScore").innerHTML = "Your Score is " + humanScore +"";
+    } else if (compScore == 5) {
+        document.getElementById("theirScore").innerHTML = "Computer Score is " + compScore +"";
+        document.getElementById("yourScore").innerHTML = "Your Score is " + humanScore +"";
+        if (confirm('The Computer has defeated you in this match, they scored ' + compScore + ' against your score of ' + humanScore + 'would you like to play again?') == true){
+            document.location.href = "/index.htm";
+        }
+        else {
+            document.location.href = "/gameover.htm";
+        }
+   
+    } else if (humanScore == 5) {
+        document.getElementById("theirScore").innerHTML = "Computer Score is " + compScore +"";
+        document.getElementById("yourScore").innerHTML = "Your Score is " + humanScore +"";
+        if (confirm('WOOOOOHOOOOOO..... You have defeated the computer this match, they scored ' + compScore + ' against your score of ' + humanScore + 'would you like to play again?') == true){
+            document.location.href = "/index.htm";
+        }
+        else {
+            document.location.href = "/gameover.htm";
+        }
+   
+}
+}
